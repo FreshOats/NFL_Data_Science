34120203e1861a18894a1322aa7a32b9e5363557
Justin Papreck <justin.papreck@gmail.com>
2024-09-17 14:48:03 -0500

    Created other pages and updated base.html

--- app/routes.py
+++ app/routes.py
@@ -10,3 +10,15 @@ def index():
 def get_injuries():
     data = scrape_injuries()
     return jsonify(data)
+
+@app.route('/biomechanics')
+def biomechanics():
+    return render_template('biomechanics.html', title='Biomechanics')
+
+@app.route('/stats')
+def stats():
+    return render_template('stats.html', title='Stats')
+
+@app.route('/visualizations')
+def visualizations():
+    return render_template('visualizations.html', title='Visualizations')
